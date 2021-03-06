a415dba3b18f808903e4df072767e8f7666cb9db
an1mebit <baldej@mail.ru>
2021-03-06 14:23:23 +0300

    hoho

--- tr.cpp
+++ tr.cpp
@@ -3,15 +3,22 @@
 using namespace std;
 int main()
 {
-    int num;
-    cin >> num;
-    unsigned long long int i = 1, bin = 0;
-    while (num > 0)
+    int num, sist;
+    cin >> num >> sist;
+    if (sist == 2)
     {
-        bin += (num % 2) * i;
-        num = num / 2;
-        i *= 10;
+        unsigned long long int i = 1, bin = 0;
+        while (num > 0)
+        {
+            bin += (num % 2) * i;
+            num = num / 2;
+            i *= 10;
+        }
+        cout << bin;
     }
-    cout << bin;
+    else if (sist == 16)
+    {
+        cout << hex << num;
+    }
     return 0;
 }
